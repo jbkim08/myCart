39dab5a85be0503d48eaa5503d26afd5a2837b29
JB <drv98@naver.com>
2025-01-21 15:34:42 +0900

    제품카테고리

--- src/components/products/ProductsSidebar.jsx
+++ src/components/products/ProductsSidebar.jsx
@@ -1,19 +1,33 @@
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import "./ProductsSidebar.css";
 import rocket from "../../assets/rocket.png";
+import apiClient from "../../utils/api-client";
+import { useEffect, useState } from "react";
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      .then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
   return (
     <aside className="products_sidebar">
       <h2>카테고리</h2>
 
       <div className="category_links">
-        <LinkWithIcon
-          title="전자제품"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories.map((c) => (
+          <LinkWithIcon
+            key={c._id}
+            title={c.name}
+            link={`products?category=${c.name}`}
+            emoji={`http://localhost:5000/category/${c.image}`}
+            sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
