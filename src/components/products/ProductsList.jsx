9c6469942ca19c1a1fde85053f7f8eaa4c4f603b
JB <drv98@naver.com>
2025-01-22 09:37:02 +0900

    useData 훅 만들기

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,17 +1,10 @@
 import ProductCard from "./ProductCard";
 import "./ProductsList.css";
-import apiClient from "../../utils/api-client";
-import { useEffect, useState } from "react";
+import useData from "../../Hook/useData";
 
 const ProductsList = () => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    apiClient
-      .get("/products")
-      .then((res) => setProducts(res.data.products))
-      .catch((err) => setError(err));
-  }, []);
+  const { data, error } = useData("products");
+  //console.log(data);
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
@@ -27,17 +20,18 @@ const ProductsList = () => {
 
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {products.map((p) => (
-          <ProductCard
-            key={p._id}
-            id={p._id}
-            image={p.images[0]}
-            price={p.price}
-            rating={p.reviews.rate}
-            ratingCounts={p.reviews.counts}
-            stock={p.stock}
-          />
-        ))}
+        {data.products &&
+          data.products.map((p) => (
+            <ProductCard
+              key={p._id}
+              id={p._id}
+              image={p.images[0]}
+              price={p.price}
+              rating={p.reviews.rate}
+              ratingCounts={p.reviews.counts}
+              stock={p.stock}
+            />
+          ))}
       </div>
     </section>
   );
--- src/components/products/ProductsSidebar.jsx
+++ src/components/products/ProductsSidebar.jsx
@@ -1,33 +1,26 @@
+import useData from "../../Hook/useData";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import "./ProductsSidebar.css";
-import rocket from "../../assets/rocket.png";
-import apiClient from "../../utils/api-client";
-import { useEffect, useState } from "react";
 
 const ProductsSidebar = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    apiClient
-      .get("/category")
-      .then((res) => setCategories(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data: categories, error } = useData("category");
+
   return (
     <aside className="products_sidebar">
       <h2>카테고리</h2>
 
       <div className="category_links">
         {error && <em className="form_error">{error}</em>}
-        {categories.map((c) => (
-          <LinkWithIcon
-            key={c._id}
-            title={c.name}
-            link={`products?category=${c.name}`}
-            emoji={`http://localhost:5000/category/${c.image}`}
-            sidebar={true}
-          />
-        ))}
+        {categories &&
+          categories.map((c) => (
+            <LinkWithIcon
+              key={c._id}
+              title={c.name}
+              link={`products?category=${c.name}`}
+              emoji={`http://localhost:5000/category/${c.image}`}
+              sidebar={true}
+            />
+          ))}
       </div>
     </aside>
   );
