d62ebddbaf0249395905e1bc51848ec4105a5307
JB <drv98@naver.com>
2025-01-22 10:19:25 +0900

    isLoading추가

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -4,7 +4,7 @@ import useData from "../../Hook/useData";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 
 const ProductsList = () => {
-  const { data, error } = useData("products");
+  const { data, error, isLoading } = useData("products");
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   //console.log(data);
   return (
@@ -22,9 +22,7 @@ const ProductsList = () => {
 
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {skeletons.map((n) => (
-          <ProductCardSkeleton key={n} />
-        ))}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
         {data.products &&
           data.products.map((p) => (
             <ProductCard
