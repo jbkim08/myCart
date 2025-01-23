1b9ff80ac19002f2fc4811848f2fbd6bd61e5bf8
JB <drv98@naver.com>
2025-01-23 14:22:44 +0900

    제품상세페이지에 장바구니 추가기능

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import { jwtDecode } from "jwt-decode";
 function App() {
   const [user, setUser] = useState(null);
   const [cart, setCart] = useState([]); //장바구니
+  const addToCart = (product, quantity) => {
+    setCart([...cart, { product, quantity }]);
+  };
+
   useEffect(() => {
     try {
       const jwt = localStorage.getItem("token");
@@ -23,7 +27,7 @@ function App() {
     <div className="app">
       <Navbar user={user} cartCount={cart.length} />
       <main>
-        <Routing />
+        <Routing addToCart={addToCart} />
       </main>
     </div>
   );
