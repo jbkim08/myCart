485a87452178d3cc9d70f9f80e307815844f2158
JB <drv98@naver.com>
2025-01-24 14:25:47 +0900

    인증된 유저만 장바구니 보이게

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import useData from "../../Hook/useData";
 import Loader from "../Common/Loader";
 import CartContext from "../../contexts/CartContext";
+import UserContext from "../../contexts/UserContext";
 
 const SingleProductPage = () => {
   const [selectedImage, setSelectedImage] = useState(0);
@@ -13,6 +14,7 @@ const SingleProductPage = () => {
   const { data: product, error, isLoading } = useData(`products/${id}`);
   //console.log(product);
   const { addToCart } = useContext(CartContext);
+  const user = useContext(UserContext);
   return (
     <section className="align_center single_product">
       {error && <em className="form_error">{error}</em>}
@@ -45,23 +47,26 @@ const SingleProductPage = () => {
             <p className="single_product_price">
               ￦ {product.price.toLocaleString("ko-KR")} 원
             </p>
+            {user && (
+              <>
+                <h2 className="quantity_title">구매개수:</h2>
+                <div className="align_center quantity_input">
+                  <QuantityInput
+                    quantity={quantity}
+                    setQuantity={setQuantity}
+                    stock={product.stock}
+                    cartPage={false}
+                  />
+                </div>
 
-            <h2 className="quantity_title">구매개수:</h2>
-            <div className="align_center quantity_input">
-              <QuantityInput
-                quantity={quantity}
-                setQuantity={setQuantity}
-                stock={product.stock}
-                cartPage={false}
-              />
-            </div>
-
-            <button
-              onClick={() => addToCart(product, quantity)}
-              className="search_button add_cart"
-            >
-              장바구니 추가
-            </button>
+                <button
+                  onClick={() => addToCart(product, quantity)}
+                  className="search_button add_cart"
+                >
+                  장바구니 추가
+                </button>
+              </>
+            )}
           </div>
         </>
       )}
