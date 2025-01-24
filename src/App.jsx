6634ab8ab06d909aa9ba867d76bd00a9e6839d4f
JB <drv98@naver.com>
2025-01-24 10:25:33 +0900

    유저 컨텍스트

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { jwtDecode } from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { addToCartAPI, getCartAPI } from "./services/cartServices";
 import { ToastContainer, toast } from "react-toastify";
+import UserContext from "./contexts/UserContext";
 
 //만약 토큰이 있으면 axios 설정에 추가됨
 setAuthToken(localStorage.getItem("token"));
@@ -51,13 +52,15 @@ function App() {
     } catch (error) {} //토큰이 없을경우는 그냥 go
   }, []);
   return (
-    <div className="app">
-      <Navbar user={user} cartCount={cart.length} />
-      <main>
-        <ToastContainer position="bottom-right" />
-        <Routing addToCart={addToCart} cart={cart} />
-      </main>
-    </div>
+    <UserContext.Provider value={user}>
+      <div className="app">
+        <Navbar user={user} cartCount={cart.length} />
+        <main>
+          <ToastContainer position="bottom-right" />
+          <Routing addToCart={addToCart} cart={cart} />
+        </main>
+      </div>
+    </UserContext.Provider>
   );
 }
 
