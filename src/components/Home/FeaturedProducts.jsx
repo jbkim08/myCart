79137d127809f7a3d6dbf6d9a3d07af9535a127e
JB <drv98@naver.com>
2025-01-31 09:42:50 +0900

    미션 홈페이지

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,15 +1,25 @@
+import useData from "../../Hook/useData";
 import ProductCard from "../products/ProductCard";
+import ProductCardSkeleton from "../products/ProductCardSkeleton";
 import "./FeaturedProducts.css";
 
 const FeaturedProducts = () => {
+  const { data: products, error, isLoading } = useData("products/featured");
+  const skeletons = [1, 2, 3];
+  //console.log(data);
+
   return (
     <section className="featured_products">
       <h2>주요제품</h2>
 
       <div className="align_center featured_products_list">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {error && <em className="form_error">{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
+        {products &&
+          !isLoading &&
+          products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
       </div>
     </section>
   );
